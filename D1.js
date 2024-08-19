913ef4cc4355a1f91a8b75310a2fdbeb2ba8496f
Kateryna Matviyeva <katiaemme96@yahoo.it>
2024-08-19 15:09:39 +0200

    Esercizi svolti

--- D1.js
+++ D1.js
@@ -11,38 +11,57 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* I datatype in javaScript sono degli oggetti racchiusi in delle scatole. Una scatola puo contere un solo oggetto e può essere un numero (intero o con il punto), 
+una stringa quindi una parola messa tra virgolette singole o doppie, può essere anche null quindi vuota o undefined che non è stata ancora aperta e infine
+può essere boolean, true o false, come se fosse un interuttore della luce, quindi esiste solo acceso o spento. */
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let myName = "Kateryna"
+console.log(myName)*/
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*let num1 = 12
+let num2 = 20
+console.log(num1 + num2)*/
+
+/*let num1 = 12
+let num2 = 20
+let somma = (num1 + num2)
+console.log(somma)*/
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*let x
+x = 12
+console.log(x)*/
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*myName = "Matviyeva"
+console.log(myName)*/
+
+/*const myColor = "black"
+myColor = "blu"
+console.log(myColor)*/
+
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
+/*let num3 = 4
+console.log(num3 - x)*/
 
-/* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -50,4 +69,11 @@ REGOLE
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*let name1 = "john"
+let name2 = "John"
+console.log(name1 !== name2)
+console.log(name1 === name2)
+
+let nameCompare = name1.toLowerCase === name2.toLowerCase
+console.log(nameCompare)*/
+
